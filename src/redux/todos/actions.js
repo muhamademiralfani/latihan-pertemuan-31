c9f26963c86b2bbedd3f8d670cc7d86f759fbf37
muhamademiralfani <muhamademiralfani@gmail.com>
2024-11-19 22:29:22 +0700

    inial commit

---
+++ src/redux/todos/actions.js
@@ -0,0 +1,25 @@
+export const ADD_TODO = 'ADD_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
+export const UPDATE_TODO = 'UPDATE_TODO';
+export const SET_EDIT_TODO = 'SET_EDIT_TODO';
+
+// action creators
+export const addTodo = (todo) => ({
+  type: ADD_TODO,
+  payload: todo,
+});
+
+export const updateTodo = (id, todos) => ({
+  type: UPDATE_TODO,
+  payload: { id, todos },
+});
+
+export const setEditTodo = (todo) => ({
+  type: SET_EDIT_TODO,
+  payload: todo,
+});
+
+export const deleteTodo = (id) => ({
+  type: DELETE_TODO,
+  payload: id,
+});
